5a735cae5a62ba0dff942849b5584b23df2bf959
MayurMallavSaikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-11 20:19:46 +0530

    Mayur Mallav Saikia

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.use(bodyParser.json());
 app.use(cors({origin:'*'}));
 
 //connection to DB in Atlas
-const {moongose}=require('../config/connection');
+const {moongose}=require('./config/connection');
 
 //Perfoming CRUD
-const user=require('../Crud/crud');
+const user=require('./Crud/crud');
 
 app.get('/',(req,res)=>{
     res.send('<table><tr><th>Method</th><th>URL</th><th>Description</th></tr><tr><td>POST</td><td>/api/users</td><td>Creates a user using the information sent inside the request body.</td></tr><tr><td>GET</td><td>/api/users</td><td>Returns an array users.</td></tr><tr><td>GET</td><td>/api/users/:id</td><td>Returns the user object with the specified id.</td></tr><tr><td>DELETE</td><td>/api/users/:id</td><td>Removes the user with the specified id and returns the deleted user.</td></tr><tr><td>PUT</td><td>/api/users/:id</td><td>Updates the user with the specified id using data from the request body. Returns the modified user</td></tr></table>')
